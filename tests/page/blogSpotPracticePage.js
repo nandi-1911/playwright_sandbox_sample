e34f311efc6f21217565f98720e073bdfe8084e0
Kowshik Preetham S <kowpreet@publicisgroupe.net>
2025-09-04 16:59:03 +0530

    added new test

--- tests/page/blogSpotPracticePage.js
+++ tests/page/blogSpotPracticePage.js
@@ -14,9 +14,26 @@ export class BlogSpotPracticePage {
         await this.page.getByPlaceholder('Enter EMail').fill(user.email);
         await this.page.getByPlaceholder('Enter Phone').fill(user.phone);
         await this.page.locator('#textarea').fill(user.address);
+        await this.page.locator('#male').check();
+
+
+        for (const day of user.days) {
+
+            // Correctly constructs a Locator object
+             await this.page.locator(`//*[@id="${day.toLowerCase()}"]`).check();
+        
+            // Use the Locator object directly to perform the action
+        }
         await this.page.locator('//button[@class="submit-btn"]').click();
+
     }
 }}
 
 
-// 
+// select files in vs code or choose using git add
+// git commit -m "added new test"
+// git push origin main
+// git checkout -b feature/blogSpotPracticePage
+// git checkout main
+// git status
+// git pull origin main
